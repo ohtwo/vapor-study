7988d55130be64715b00309b200abf85f555eff8
Kang Byeonghak <hello@tinygram.dev>
2022-08-11 19:19:02 +0900

    1-2 Hello Vapor!

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,27 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
+  app.get { req async in
+    "It works!"
+  }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+  app.get("hello", ":name") { req async throws -> String in
+    guard let name = req.parameters.get("name") else {
+      throw Abort(.internalServerError)
     }
+    return "Hello, \(name)!"
+  }
+  
+  app.post("info") { req -> InfoResponse in
+    let data = try req.content.decode(InfoData.self)
+    return InfoResponse(request: data)
+  }
+}
+
+struct InfoData: Content {
+ let name: String
+}
+
+struct InfoResponse: Content {
+ let request: InfoData
 }
